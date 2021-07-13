2d4f514591d53b2327c3397447137cc115fc9b36
manoj k <manoj0134@gmail.com>
2021-07-12 23:30:57 -0400

    convert Bike class POJO to JPA entity

--- bike/src/main/java/com/globomatics/bike/models/Bike.java
+++ bike/src/main/java/com/globomatics/bike/models/Bike.java
@@ -1,9 +1,20 @@
 package com.globomatics.bike.models;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.sql.Date;
 
+@Entity
 public class Bike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String email;
     private String phone;
@@ -75,5 +86,13 @@ public class Bike {
 
     public void setContact(boolean contact) {
         this.contact = contact;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
