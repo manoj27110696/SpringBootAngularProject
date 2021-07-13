cf4e5fd6a997dc62684c95a18df4497461be7620
manoj k <manoj0134@gmail.com>
2021-07-12 23:07:04 -0400

    Initial commit

---
+++ bike/src/main/java/com/globomatics/bike/models/Bike.java
@@ -0,0 +1,79 @@
+package com.globomatics.bike.models;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class Bike {
+    private String name;
+    private String email;
+    private String phone;
+    private String model;
+    private String serialNumber;
+    private BigDecimal purchasePrice;
+    private Date purchasedate;
+    private boolean contact;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public BigDecimal getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(BigDecimal purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Date getPurchasedate() {
+        return purchasedate;
+    }
+
+    public void setPurchasedate(Date purchasedate) {
+        this.purchasedate = purchasedate;
+    }
+
+    public boolean isContact() {
+        return contact;
+    }
+
+    public void setContact(boolean contact) {
+        this.contact = contact;
+    }
+}
